63ccc8bfe59b98ce65aa4cb6d13ee838b5d1401f
Maxim Kostyukov <maximkostyukov@yandex.ru>
2022-07-02 14:39:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
         "numpy",
         "matplotlib",
         "joblib",
-        "ensemble @ git+https://github.com/Anessivan/Neuro@master",
+        "ensemble @ git+https://github.com/Anessivan/Neuro@anessivan/coupling_normalization",
     ],
 )
